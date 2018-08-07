25fac67e804b96fb5a4564a3e6969c1cc718a630
Henrik Brustad <henrik.granheim.brustad@finn.no>
2018-08-07 11:47:31 +0200

    Initial Commit

---
+++ MosaicLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MosaicLayout
+//
+//  Created by Granheim Brustad , Henrik on 07/08/2018.
+//  Copyright © 2018 Henrik Brustad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
